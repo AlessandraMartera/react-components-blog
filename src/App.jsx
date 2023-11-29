d2666de67811ea0805b1e91cd3e31674f5299c60
alessandra martera <ale.martera@gmail.com>
2023-11-29 14:14:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import TheHeader from './components/TheHeader'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <TheHeader/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TheHeader.jsx
@@ -0,0 +1,10 @@
+export default function TheHeader (){
+    return (
+        <>
+        <header>
+            <h1>titolo</h1>
+        </header>
+    </>
+    )
+    
+}
